990009c828885e4b9feb8826ea75feb7eed85e11
arminhammer <arminhammer@gmail.com>
2015-01-11 12:54:21 -0500

    Fixed dropdown tab menu issue.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -5,14 +5,8 @@ angular.module('schwanzen')
 
     $scope.tabs = [
       { title:'Dynamic Title 1', content:'Dynamic content 1' },
-      { title:'Dynamic Title 2', content:'Dynamic content 2' }
+      { title:'Dynamic Title 2', content:'Dynamic content 2' },
     ];
-
-    $scope.alertMe = function() {
-      setTimeout(function() {
-        $window.alert('You\'ve selected the alert tab!');
-      });
-    };
 
     $scope.items = [
       'The first choice!',
