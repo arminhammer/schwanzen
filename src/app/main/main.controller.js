4c6a928d819cb97bfcc4166738d5019b52a5e378
arminhammer <arminhammer@gmail.com>
2015-01-17 20:06:54 -0500

    Minor css changes to the line table.

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -37,8 +37,18 @@ angular.module('schwanzen')
     $scope.tabs['Dynamic Title 2'] = {
       filename:'Dynamic Title 2',
       lines: [
+        'Line 1',
+        'Line 2',
         'Line 3',
-        'Line 4'
+        'Line 3',
+        'Line 4',
+        'Line 5',
+        'Line 6',
+        'Line 7',
+        'Line 8',
+        'Line 9',
+        'Line 10',
+        'Line 11'
       ],
       newLines: 5
     };
