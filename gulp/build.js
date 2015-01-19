430a7109142973e92d5b49aeebd67808ae36b4e4
arminhammer <arminhammer@gmail.com>
2015-01-18 22:47:05 -0500

    Fixed missing setting glyph image.

--- gulp/build.js
+++ gulp/build.js
@@ -95,6 +95,7 @@ gulp.task('images', function () {
 });
 
 gulp.task('fonts', function () {
+  console.log($.mainBowerFiles());
   return gulp.src($.mainBowerFiles())
     .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
     .pipe($.flatten())
